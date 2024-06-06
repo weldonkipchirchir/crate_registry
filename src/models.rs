6a63a6b9fa30aff698cd15b8d39c7f4a7314fd20
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-06-07 00:00:01 +0300

    add models

--- src/models.rs
+++ src/models.rs
@@ -1,43 +1,44 @@
-use chrono::{NaiveDate, NaiveDateTime};
 use crate::schema::*;
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
 
 //only for existing record
-#[derive(Queryable)]
-struct Rustacean{
-    id:i32,
-    name: String,
-    email:String,
-    created_at:NaiveDateTime
-}
-
-
-//add a record
-#[derive(Insertable)]
-#[table_name="rustaceans"]
-struct NewRustacean {
-    name: String,
-    email:String,
-}
-
-//only for existing record
-#[derive(Queryable, Associations)]
-struct Crate{
-    id:i32,
-    rustacean_id:i32,
-    code: String,
-    name:String,
-    version:String,
-    description: Option<String>,
-    created_at:NaiveDateTime   
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct Rustacean {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+    pub created_at: NaiveDateTime,
 }
 
 //add a record
-#[derive(Insertable)]
-#[table_name="crates"]
-struct NewCrate{
-    rustacean_id:i32,
-    code: String,
-    name:String,
-    version:String,
-    description: Option<String>,
+#[derive(Insertable, Deserialize)]
+#[table_name = "rustaceans"]
+pub struct NewRustacean {
+    pub name: String,
+    pub email: String,
+}
+
+//only for existing record
+#[derive(Queryable, Associations, Serialize, Deserialize)]
+#[belongs_to(Rustacean)]
+pub struct Crate {
+    pub id: i32,
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub created_at: NaiveDateTime,
+}
+
+//add a record
+#[derive(Insertable, Deserialize)]
+#[table_name = "crates"]
+pub struct NewCrate {
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
 }
