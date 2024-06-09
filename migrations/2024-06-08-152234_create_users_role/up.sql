102e67cddcbeb7035d7cc7b1cbc678e479cec3a9
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-06-09 23:44:26 +0300

    add users_roles migration

---
+++ migrations/2024-06-08-152234_create_users_role/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE users_roles (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id),
+    role_id integer NOT NULL REFERENCES roles(id)
+);
