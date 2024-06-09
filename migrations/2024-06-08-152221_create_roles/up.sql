e66b48d47445c40fa080806e731b8ae9f8fc499e
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-06-09 23:43:47 +0300

    add roles migration

---
+++ migrations/2024-06-08-152221_create_roles/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE roles(
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(64) NOT NULL UNIQUE,
+    name VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
